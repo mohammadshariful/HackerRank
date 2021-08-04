501e4aa21f29f785533c624f93894c217d0e7fcf
sharif64 <mohammadsharifulislam64@gmail.com>
2021-08-04 18:14:45 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+let navLinks = document.getElementById("navLinks");
+function openMenu() {
+    navLinks.style.left ="0";
+}
+function closeMenu() {
+    navLinks.style.left ="-200px";
+}
